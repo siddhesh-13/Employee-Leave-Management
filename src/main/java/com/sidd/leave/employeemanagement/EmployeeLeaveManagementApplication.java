34921e44e9c80d84d0d28873b4a3ff0b94930048
siddhesh-13 <siddheshghadigaonkar110@gmail.com>
2025-08-18 18:44:08 +0530

    first commit

---
+++ src/main/java/com/sidd/leave/employeemanagement/EmployeeLeaveManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sidd.leave.employeemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeLeaveManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeLeaveManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidd/leave/employeemanagement/EmployeeLeaveManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidd.leave.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeLeaveManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
